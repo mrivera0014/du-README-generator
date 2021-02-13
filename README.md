a28af0c63d768c80e808297167c47ee0132b89f3
Marble <mrivera0014@gmail.com>
2021-02-12 20:33:59 -0700

    created prompt function, all questions are running, README function done, need answers to be inserted in README file

--- README.md
+++
@@ -1,1 +0,0 @@
-# du-README-generator
--- index.js
+++ index.js
@@ -1,56 +1,122 @@
 const inquirer = require('inquirer')
 const fs = require('fs')
+const util = require('util');
 
-const questions = inquirer
-    .prompt([
+const writeFileAsync = util.promisify(fs.writeFile)
+
+const questions = () => {
+    return inquirer.prompt([
         {
+            //ask for project name
             type: "input",
             message: "What is the title of your project?",
             name: "Title"
         },
         {
+            //short description of project
             type: "input",
             message: "Enter a description about your project",
             name: "Description"
         },
-        // {
-        //     type: "input",
-        //     message: "Something will go here",
-        //     name: "Table of Contents"
-        // },
-        // {
-        //     type: "input",
-        //     message: "What modules were used for this project?",
-        //     name: "Installation"
-        // },
+        {
+            //licenses project should have
+            //will have choices to tab up and down--default answer? can be overridden
+            type: "list",
+            message: "What kind of license does/should your project have?",
+            choices: ["MIT", "Apache 2.0", "Boost", "Other", "None"],
+            name: "License"
+        },
+        {
+            //lets user know what command to run to install module
+            type: "input",
+            message: "What command should be run to install dependencies?",
+            default: "npm i",
+            name: "Installation"
+        },
+        {
+            //lets user know what command they need to run to run tests
+            type: "input",
+            message: "What command should be run to run tests?",
+            default: "npm test",
+            name: "Tests"
+        },
         {
             type: "input",
-            message: "How can this application be used?",
+            message: "What should the user know about using the repo?",
             name: "Usage"
         },
-        // {
-        //     type: "input",
-        //     message: "What license(s) were used for this application?",
-        //     name: "License"
-        // },
-        // {
-        //     type: "input",
-        //     message: "",
-        //     name: "Contribution Guidelines"
-        // },
-        // {
-        //     type: "input",
-        //     message: "",
-        //     name: "Tests"
-        // },
         {
             type: "input",
-            message: "Enter your GitHub username",
-            name: "Questions"
+            message: "What should the user know to contribute to the repo?",
+            name: "Contribution Guidelines"
         },
         {
+            //ask for github username
+            type: "input",
+            message: "Enter your GitHub username",
+            name: "GitHub"
+        },
+        {//ask for email
             type: "input",
             message: "Enter your email",
-            name: "Questions"
+            name: "Email"
         }
     ])
+}
+
+const generateReadMe = (answer) =>
+    `# Readme
+<p>${answer.Title}</p>
+## Description
+<p>${answer.Description}</p>
+
+## Table of Contents
+
+
+## Installations
+
+
+## Usage
+
+
+## License
+
+
+## Contributing
+
+
+## Tests
+
+
+## Questions`;
+
+const init = () => {
+    questions()
+        .then((answer) => writeFileAsync('README.md', generateReadMe(answer)))
+        .then(() => console.log('It works??'))
+        .catch((err) => console.error(err))
+
+}
+
+init()
+
+    //what does user need to know about using the repo?
+    //what does user need to know about contributing to the repo?
+    //this will then generate the readme file
+
+    //readme
+
+    //table of contents
+
+    //installation
+
+    //usage
+
+    //license--make this a list of choices
+    // [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+    //tests -- EX: To run tests, run the following command
+
+    // --- node index.js ----
+
+    //questions -- EX: If you have any questions about the repo, contact me directly at mrivera0014@gmail.com. You can find more of my work at [mrivera0014]{github link in here}
