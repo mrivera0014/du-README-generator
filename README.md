6b61d08fe28754279d0e195c129007769230b003
Marble <mrivera0014@gmail.com>
2021-02-13 14:33:35 -0700

    rearranged generateMarkdown.js , READMe file being generated now!

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-#undefined
+# Readme
+t
+
+## Description
+t
+
+## Table of Contents
+
+
+## Installations
+To install necessary dependencies, run the following command followed by the module name:
+
+t
+
+## Usage
+t
+
+## License
+
+
+## Contributing
+t
+
+## Tests
+To run tests run the following command:
+
+t
+
+## Questions
+If you have any questions contact me at t.
+Find more of my work at (https://github.comt/)
--- util/generateMarkdown.js
+++ util/generateMarkdown.js
@@ -33,42 +33,42 @@ ${answer.Tests}
 If you have any questions contact me at ${answer.Email}.
 Find more of my work at (https://github.com${answer.GitHub}/)`;
 
-function renderLicenseBadge(License) {
-    if (License === "MIT") {
-        return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`
-    } else if (License === "Apache 2.0") {
-        return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
-    } else if (License === "Boost") {
-        return `[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`
-    } else (License === "None"); {
-        return ``
-    }
-}
+// function renderLicenseBadge(License) {
+//     if (License === "MIT") {
+//         return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`
+//     } else if (License === "Apache 2.0") {
+//         return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+//     } else if (License === "Boost") {
+//         return `[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`
+//     } else (License === "None"); {
+//         return ``
+//     }
+// }
 
-function renderLicenseLink(License) {
-    if (License === "MIT") {
-        return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`
-    } else if (License === "Apache 2.0") {
-        return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
-    } else if (License === "Boost") {
-        return `[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`
-    } else (License === "None"); {
-        return ``
-    }
+// function renderLicenseLink(License) {
+//     if (License === "MIT") {
+//         return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`
+//     } else if (License === "Apache 2.0") {
+//         return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+//     } else if (License === "Boost") {
+//         return `[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`
+//     } else (License === "None"); {
+//         return ``
+//     }
 
-}
+// }
 
-function renderLicenseSection(License) {
-    if (License === "MIT") {
-        return `This project is licensed under the MIT license.`
-    } else if (License === "Apache 2.0") {
-        return `This project is licensed under the Apache 2.0 license.`
-    } else if (License === "Boost") {
-        return `This project is licensed under the Boost Software 1.0 license.`
-    } else (License === "None"); {
-        return ``
-    }
-}
+// function renderLicenseSection(License) {
+//     if (License === "MIT") {
+//         return `This project is licensed under the MIT license.`
+//     } else if (License === "Apache 2.0") {
+//         return `This project is licensed under the Apache 2.0 license.`
+//     } else if (License === "Boost") {
+//         return `This project is licensed under the Boost Software 1.0 license.`
+//     } else (License === "None"); {
+//         return ``
+//     }
+// }
 
 function generateMarkdown(answer) {
     return `#${answer.license}`
@@ -78,6 +78,6 @@ function generateMarkdown(answer) {
 
 
 
-module.exports = generateMarkdown;
+module.exports = generateReadMe;
 
 
