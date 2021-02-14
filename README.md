436c7cbbe61b5a5f272a534b934817e5a5c66ae9
Marble <mrivera0014@gmail.com>
2021-02-14 12:56:08 -0700

    testing renderLicenseSection function

---
+++ README.md
@@ -0,0 +1,30 @@
+#README-Generator 
+
+## Description
+Creates a README file based on user input.
+
+## Table of Contents
+
+
+## Installations
+npm i
+
+
+## Usage
+N/A
+
+## License
+
+[![License: MIT]](https://opensource.org/licenses/MIT)]
+
+## Contributing
+Contact me
+
+## Tests
+To run tests run the following command:
+
+npm test
+
+## Questions
+If you have any questions contact me at mrivera0014@gmail.com.
+Find more of my work at (https://github.commrivera0014/)
--- util/generateMarkdown.js
+++ util/generateMarkdown.js
@@ -1,23 +1,9 @@
 const index = require('../index')
 
 
-function renderLicenseBadge(License) {
+function renderLicenseSection(License) {
     if (License === "MIT") {
-        return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]`
-    } else if (License === "Apache 2.0") {
-        return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]`
-    } else if (License === "Boost") {
-        return `[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)]`
-    } else (License === "None"); {
-        return ``
-    }
-}
-
-
-
-function renderLicenseLink(License) {
-    if (License === "MIT") {
-        return `[![License: MIT](https://opensource.org/licenses/MIT)]`
+        return `[![License: MIT]](https://opensource.org/licenses/MIT)]`
     } else if (License === "Apache 2.0") {
         return `[![License](https://opensource.org/licenses/Apache-2.0)]`
     } else if (License === "Boost") {
@@ -25,28 +11,36 @@ function renderLicenseLink(License) {
     } else (License === "None"); {
         return ``
     }
-
 }
 
-function renderLicenseSection(License) {
-    if (License === "MIT") {
-        return `This project is licensed under the MIT license.`
-    } else if (License === "Apache 2.0") {
-        return `This project is licensed under the Apache 2.0 license. `
-    } else if (License === "Boost") {
-        return `This project is licensed under the Boost Software 1.0 license.`
-    } else (License === "None"); {
-        return ``
-    }
-}
-renderLicenseSection()
-// console.log("LicenseSection works?") it works so far..
 
+
+// function renderLicenseLink(License) {
+//     if (License === "MIT") {
+//         return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]`
+//     } else if (License === "Apache 2.0") {
+//         return `[![License](https://opensource.org/licenses/Apache-2.0)]`
+//     } else if (License === "Boost") {
+//         return `[![License](https://www.boost.org/LICENSE_1_0.txt)]`
+//     } else (License === "None"); {
+//         return ``
+//     }
+
+// }
+
+// function renderLicenseSection(License) {
+//     if (License === "MIT") {
+//         return `This project is licensed under the MIT license.`
+//     } else if (License === "Apache 2.0") {
+//         return `This project is licensed under the Apache 2.0 license.`
+//     } else if (License === "Boost") {
+//         return `This project is licensed under the Boost Software 1.0 license.`
+//     } else (License === "None"); {
+//         return ``
+//     }
+// }
 const generateReadMe = (answer) =>
-    `${renderLicenseBadge(answer.License)}
-
-#${answer.Title}
-
+    `#${answer.Title} 
 
 ## Description
 ${answer.Description}
@@ -62,8 +56,8 @@ ${answer.Installation}
 ${answer.Usage}
 
 ## License
+
 ${renderLicenseSection(answer.License)}
-${renderLicenseLink(answer.License)}
 
 ## Contributing
 ${answer.Contribution}
@@ -83,5 +77,5 @@ Find more of my work at (https://github.com${answer.GitHub}/)`;
 
 module.exports = generateReadMe;
 
-
-// [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+// ${ renderLicenseSection(answer.License) }
+// ${ renderLicenseLink(answer.License) }
